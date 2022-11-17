3c1a0a5338798848794ee60f099e40ac232aa209
yashgupttaa <yashgupta365ji@gmail.com>
2022-11-17 12:07:36 +0530

    my first commit

---
+++ number.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<link rel="stylesheet" href="style.css">
+    <title>Document</title>
+    <style>
+        table, td, th {
+    border: 1px solid;
+    text-align:center;
+    
+  }
+  table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 1rem;
+    background:white;
+
+  }
+    </style>
+</head>
+<body style="background:black;">
+    <div class="top">
+    <marquee direction="scroll">Sadak, Sadak result, Gali result, Disawar result, Sadak chart, Sadak live, Gali satta, Disawar live result, Gali live result, disawar, Gali live result, Disawar live result, Sadak Number, Sadak Game, Gali Number, Sadak Bazar, Gali Single Jodi, Black Sadak Result, Disawar Single Jodi</marquee>
+    </div>
+    <div class="container-fluid p-0">
+        <!-- Result -->
+        <div class="showNum text-center text-white p-2" style="background:black;">
+             <p id="date" class="text-warning"></p>
+            <h1>SADAK</h1>
+            <h1 class="text-warning">{{86}}</h1>
+            
+            <h1>DISAWER</h1>
+            <h1 class="text-warning">{{86}}</h1>
+            <h1>GALI</h1>
+            <h1 class="text-warning">{{86}}</h1>
+            <h1>GHAZIABAD</h1>
+            <h1 class="text-warning">{{86}}</h1>
+            <h1>FARIDABAD</h1>
+            <h1 class="text-warning">{{86}}</h1>
+        </div>
+        <!-- Result End -->
+        <div style="width=100%; height:1.5rem; background:white;"></div>
+
+        <!-- contact -->
+        <div class="contact1 text-center mt-3">
+            <h5 class="mt-1">सभी सट्टा खेलने वाले भाईयो के लिए खुश खबरी सड़क; गली; दिसावर; गाजियाबाद ;और फरीदाबाद; में होगा सिंगल जोड़ी ब्लास्ट तो जिन भाईयो को लाखो का लॉस है होगा अब एक ही बार में कवर तो बिना समय निकालें जल्दी से जल्दी वॉट्सएप पर जुड़े और कमाए लाखो रुपए</h5>
+            <h5>king</h5>
+            <h5>1111111111</h5>
+        </div>
+        <div class="contact2 text-center mt-3">
+            <h5 class="mt-1">Sadak Gali Disawar Gaziyabad Faridabad डेली गेम पास फुल गारंटी के साथ जिसको अपना लॉस कवर करना है वो whatsapp करे हम बोलते नहीं हम कर के दिखाते है Note-जो भाई मोटा कामना चाहते है और जो मोटा खेलते है केवल उनके लिए</h5>
+            <h5>kohli</h5>
+            <h5>2222222222</h5>
+        </div>
+        <!-- end -->
+
+        <!-- CARDS -->
+       <div class="cards mt-2">
+        
+
+        <div class="menu">
+            <h4 class="m-0"><a href="#" class="text-white">SADAK</a></h4>
+            <p id="time" class="m-0">( 2:15 AM )</p>
+            <h4>{20} OR [38]</h4>
+        </div>
+        <div class="menu">
+            <h4 class="m-0"><a href="#" class="text-white">DISAWER</a></h4>
+            <p id="time" class="m-0">( 05:00 AM )</p>
+            <h4>{20} OR [38]</h4>
+        </div>
+       </div>
+       <div class="cards">
+        <div class="menu">
+            <h4 class="m-0"><a href="#" class="text-white">GALI</a></h4>
+            <p id="time" class="m-0">( 11:15 PM )</p>
+            <h4>{20} OR [38]</h4>
+        </div>
+        <div class="menu">
+            <h4 class="m-0"><a href="#" class="text-white">GHAZIABAD</a></h4>
+            <p id="time" class="m-0">( 08:00 PM )</p>
+            <h4>{20} OR [38]</h4>
+        </div>
+        </div>
+        <div class="cards">
+        <div class="menu">
+            <h4 class="m-0"><a href="#" class="text-white">FARIDABAD</a></h4>
+            <p id="time" class="m-0">( 06:15 PM )</p>
+            <h4>{20} OR [38]</h4>
+        </div>
+        </div>
+        
+       </div>
+        <!-- end cards -->
+
+        <!-- Result Table -->
+
+        
+        <table>
+            
+            <tr>
+                <th>DATE</th>
+                <th>SADAK</th>
+                <th>DISAWER</th>
+                <th>GALI</th>
+                <th>GHAZIABAD</th>
+                <th>FARIDABAD</th>
+            </tr>
+            <tbody>
+            <?php
+              for ($i=1; $i <= 30 ; $i++) { 
+                echo "<tr><td>".$i."/11"."</td>
+                <td>"."</td>
+                <td>"."</td>
+                <td>"."</td>
+                <td>"."</td>
+                <td>"."</td>
+                </tr>";
+                
+              }
+            ?>
+            </tbody> 
+        </table>
+        
+
+        <!-- Result Table End -->
+
+        
+
+        <!-- WARNING -->
+        <div class="p-2">
+        <div class="warning text-center mt-3">
+            <h1 class="mb-3">How to play SADAK ?</h1>
+            <p>In SADAK, people wager bets on their chosen numbers between 0 and 99. For this the bettors contact their area’s Khaiwal. Khaiwal works as a mediator/middle man between the bettors and the game operators. Every Khaiwal collects money and the number from the players of his area and sends it to the company. And once a winner is declared he collects the winnings from the company and delivers it to the winning bettor.</p>
+            <p>At a predefined time the SADAK(सड़क) company opens a random number(SADAK result). The winning bettor receives 90 times the money he wagered on a wining number.
+            </p><br>
+            <h1 class="mb-3">Where to get SADAK result ?</h1>
+            <p>In today's internet world, Getting a Sadak Result very fast not a big deal. You will find many websites on internet which provides live sadak result on their portal. On these website you will get fastest gali result, disawar result, faridabad result, gaziyabad result, etc. All you have to do just search satta result, gali result, disawar result, faridabad result, gaziyabad result on google and you will get live sadak result instantly online.</p>
+            <br>
+            <h1 class="mb-3">How to find a perfact winning number?</h1>
+            <p>To make money from Sadak game all you need is a winning number. There is not mathematical formula to draw this number. People just guess the next number by looking at the old Sadak record chart of any game. Hence, they play many numbers at the same time.
+               You will find many Bookies who according to their calculations sell their predicted numbers(Sadak result) to the players. They charge a big fee for this. But it is not necessary that their number will be the winning number. They also guess based on their experience and their guesses(Sadak result) are many times correct as well.
+            </p><br>
+            <h1 class="mb-3">How much profit you can earn from Sadak?
+            </h1>
+            <p>Suppose if someone has bet 10 rupees on a number, then if that number is opened then the user will get 10 x 90 = 900 Rupees. Similarly, users will get 1800 rupees for 20 rupees, 2700 rupees for 30 rupees, 3600 rupees for 40 rupees and 4500 rupees for 50 rupees. The user can invest as much money as he wants on one number and can play many numbers as he wants.</p><br>
+            <h1 class="mb-3">What happen when you play Sadak ?</h1>
+            <p>Most people have the misconception that if they play Sadak(सड़क), they will win and earn good amount of money. But this happens quite the opposite. Those people get trapped in such game so badly that they loses everything and get ruined.</p><br>
+            <p>As you know, only one number(Sadak) from 00 to 99 opens in this game. This means that in this game, out of 100 people only one man gets a lottery. The remaining 99 people only loses in this game. And the whole money of 99 losers is given to the winner. The chances of winning in this game are 1% out of 100. People know about this but still they want to play this game till they are completely ruined.
+            </p><br>
+            <h1 class="mb-3">What is the Reality of Sadak game ?</h1>
+            <p>Sadak(सड़क) is a type of lottery game. In which 100 people take part on their free will. Out of which one person is chosen as winner between 1 to 100 numbers and for this, all 100 numbers are put in a pot. Then a slip of any number is drawn. The person who has the number on the slip will be the winner. Actually, this is how people described this game but nothing like this happens in this game. In fact in this game, Sadak Company does not pick the slip of any one number by putting the numbers in the pot, but only the number which has less money invested on it is drawn out of 100 numbers. Because the Sadak company runs to business to make the most profit for themselves. No number is randomly opened. The number is fixed by the Sadak company. Which means that the team of Sadak gets a lot of profit, which means who will be the winner is depends on the team of Sadak.</p><br>
+            <h1 class="mb-3">Disclaimer
+            </h1>
+            <p>Sadak.in is a non commercial website. Viewing This WebSite Is Your Own Risk. All The Information Shown On Website Is Sponsored And We Warn You That Matka Gambling/Sadak May Be Banned Or Illegal In Your Country. We Don't Run Any Sadak Gambling And Don't Have Any Connection With Sadak/Sadak Company In Any Way. If Some One Ask You To Pay Money Please Take Decision as per your understanding. We will be Not Responsible For Any Issues Or Scam. We Respect All Country Rules/Laws. If You Not Agree With Our Site Disclaimer. Please Quit Our Site Right Now. Thanks
+            </p>
+            <h1 class="mb-3">चेतावनी</h1>
+            <p>एक नॉन कमर्शियल वेबसाइट है। इस वेबसाइट को देखना आपका अपना जोखिम है। वेबसाइट पर दिखाई गई सभी जानकारी प्रायोजित है और हम आपको चेतावनी देते हैं कि मटका जुआ/सड़क आपके देश में प्रतिबंधित या अवैध हो सकता है। हम कोई सट्टा मटका जुआ नहीं चलाते हैं और किसी भी तरह से सट्टा मक्ता / सड़क कंपनी से कोई संबंध नहीं है। यदि कोई आपसे पैसा मांगे तो कृपया अपनी समझ के अनुसार निर्णय लें। हम किसी भी मुद्दे या घोटाले के लिए जिम्मेदार नहीं होंगे। हम देश के सभी नियमों/कानूनों का सम्मान करते हैं। यदि आप हमारी साइट के अस्वीकरण से सहमत नहीं हैं। कृपया अभी हमारी साइट छोड़ दें। धन्यवाद</p>
+        </div>
+        </div>
+        <!-- WARNING END -->
+
+        <div class="top">
+           <marquee direction="scroll">महत्वपूर्ण:इस गेम में वित्तीय जोखिम शामिल है और यह व्यसनकारी हो सकता है। कृपया जिम्मेदारी से और अपने जोखिम पर खेलें।</marquee>
+         </div>
+         <div class="footer text-center text-white mt-4 mb-4">
+            <p style="font-size:20px;">Copyright © Sadak
+            </p>
+         </div>
+
+    </div>
+    <div class="text-center contact3 mb-3">
+        <h5>सभी सट्टा खेलने वाले भाईयो के लिए खुश खबरी गली; दिसावर; गाजियाबाद ;और फरीदाबाद; में होगा सिंगल जोड़ी ब्लास्ट तो जिन भाईयो को लाखो का लॉस है होगा अब एक ही बार में कवर तो बिना समय निकालें जल्दी से जल्दी वॉट्सएप पर जुड़े और कमाए लाखो रुपए</h5>
+        <h5 class="text-success">virat</h5>
+        <h5>4444444444</h5>
+    </div>
+
+    <script>
+        document.getElementById('date').innerHTML=Date();
+    </script>
+</body>
+</html>
